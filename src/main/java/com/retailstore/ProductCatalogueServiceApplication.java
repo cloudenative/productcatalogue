1bb8c2bf1273d01d1ceba4526eb1297d700751e6
Raja Roy <rroy@prokarma.com>
2016-08-03 16:35:24 +0000

    Initial Commit

---
+++ src/main/java/com/retailstore/ProductCatalogueServiceApplication.java
@@ -0,0 +1,12 @@
+package com.retailstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogueServiceApplication.class, args);
+    }
+}
